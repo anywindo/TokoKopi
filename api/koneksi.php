f4f3a03a6f8911aa7bcae0a40b05dc145d87d8f8
DustInTheWind <arw.prt@gmail.com>
2025-12-04 21:36:33 +0700

    K̸̤̱͔̝͎̣͒͌̂͘y̷͙̻̝̦̞̞̟̋͐̾͝͠s̷͙̟͉̣̰̙̩̝̓̃̋̔̕͜͝ͅ

--- api/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Membuat koneksi ke database MySQL
-// Host: localhost, User: root, Password: (kosong), Database: pwd_coffeeshop
-$conn = mysqli_connect("localhost", "root", "", "pwd_coffeeshop");
-
-if (!$conn) {
-    die("Gagal koneksi: " . mysqli_connect_error());
-}
--- api/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Mulai sesi untuk mengakses data sesi saat ini
-session_start();
-// Hapus semua data sesi untuk logout user
-session_destroy();
-header('Location: ../index.xhtml');
-exit();
-?>
